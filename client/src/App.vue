36dcaa7dfa044b33f448224b5920e86231812333
itays123 <itays2005@gmail.com>
2020-08-17 16:24:08 +0300

    setup navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from '@/router/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ client/src/router/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <div class="brand">
+      <router-link to="/">
+        <img alt="logo" src="../assets/logo.png" width="24" height="24" />
+        <h1>Point Of Vue</h1>
+      </router-link>
+    </div>
+    <div class="categories">
+      <router-link to="/category/1">Israel News</router-link> |
+      <router-link to="/category/2">World</router-link> |
+      <router-link to="/category/3">COVID-19</router-link> |
+      <router-link to="/category/4">Middle East</router-link> |
+      <router-link to="/category/5">Tech & Space</router-link> |
+      <router-link to="/category/6">Economy</router-link> |
+      <router-link to="/category/7">Environment</router-link> |
+      <router-link to="/category/8">Travel</router-link>
+    </div>
+  </div>
+</template>
+ּּ
+<style scoped>
+#nav {
+  background: #00838f;
+  color: #fff;
+}
+
+#nav a {
+    text-decoration: none;
+    color: inherit;
+}
+
+.brand a {
+  display: flex;
+  height: 3.5rem;
+  align-items: center;
+  justify-content: center;
+}
+
+.brand h1 {
+  margin-left: 8px;
+  font-family: "Lobster";
+}
+
+.categories {
+    padding-bottom: 8px;
+}
+</style>
